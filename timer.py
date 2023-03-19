2ee9b5438d721ec680741ef310440aaed1ae1db3
Sergei Repin <72044728+404smoke@users.noreply.github.com>
2023-03-20 02:06:03 +0300

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,10 +1,11 @@
 import time
 x=1
+yyy=123
 f=open("/var/www/index.html", "w+")
 while x>0:
         x=x+1
         f.write(' ')
         f.seek(0)
-        f.write("<html><br><br><center>Container is up for "+str(x)+" seconds</center><script>setTimeout(function(){location.reload();}, 1000);</script></html>")
+        f.write("<html><br><br><center>Container is up for "+str(x)+" seconds<br>"+str(yyy)+"</center><script>setTimeout(function(){location.reload();}, 1000);</script></html>")
         time.sleep(1)
 f.close()
