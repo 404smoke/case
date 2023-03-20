e1cf7bddea8ebf151c13d9c55d3b6bc48cc7a781
Sergei Repin <72044728+404smoke@users.noreply.github.com>
2023-03-20 03:28:42 +0300

    Update change.sh

--- change.sh
+++ change.sh
@@ -5,4 +5,4 @@ sed -i "s/yyy=.*/yyy=\'$TTT\'/g" timer.py
 docker stop timer
 docker container rm timer
 docker build -t timer .
-docker run -d -v www_data:/var/www timer
+docker run --name=timer -d -v www_data:/var/www timer
